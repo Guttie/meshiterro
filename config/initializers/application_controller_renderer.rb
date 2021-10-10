1c85588c5a26337b4573fda9aa3ab330e3d74552
YutaroSakaguchi <yutarosakaguchi@gmail.com>
2021-10-10 09:44:52 +0000

    Finish [8-4]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2893f5b4229e7ac958cab663ced6f67c19219318b399a3fa0e2ab3c58fd8e4e2ca7898e191264c808650bbbf0ec3a37db5e8a9c00e5eb7d437a63e1b31db4d38'
